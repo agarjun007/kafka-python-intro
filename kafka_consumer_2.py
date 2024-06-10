ed8ea9ba4cdecec09b12ea239593dfc17121be70
Arjun A.G <arjun.ag@fayausa.com>
2024-06-10 09:46:28 +0530

    change in consumer and producer

--- kafka_consumer_2.py
+++ kafka_consumer_2.py
@@ -2,14 +2,14 @@ from kafka import KafkaConsumer
 
 # Configure Kafka
 bootstrap_servers = 'localhost:29092'
-# group_id = 'my-group'
+group_id = 'my-group'
 
 # Define the topic to which you want to receive messages
 topic = "demo-topic"
 
 # Create a Kafka consumer
 consumer = KafkaConsumer(topic,
-                        #  group_id=group_id,
+                         group_id=group_id,
                          bootstrap_servers=bootstrap_servers)
 
 # Define a function to handle messages
